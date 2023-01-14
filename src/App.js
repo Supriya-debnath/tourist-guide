d0377689e27a455b7138ef12c4c172dbc4466e43
Priyanka Debnath <priyanka.wdebnath@gmail.com>
2023-01-15 02:39:55 +0600

    tourist guide

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import OurServices from './Components/OurServices/OurServices';
+import About from './Components/AboutUs/About';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Destination from './Components/Destination/Destination';
+import Contact from './Components/ContactUs/Contact';
+import OurBlogs from './Components/OurBlogs/OurBlogs';
+import Login from './Components/Login/Login/Login';
+import Registration from './Components/Login/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header></Header>
+      <Routes>
+          <Route exact path="/" element={<Home />}>
+          </Route>
+
+          <Route path="/home" element={<Home />}>
+          </Route>
+
+          <Route path="/destination" element={ <Destination></Destination>}>
+          </Route>
+
+          <Route path="/about" element={<About></About>}>
+          </Route>
+
+          <Route path="/services" element={ <OurServices></OurServices>}>
+         </Route>
+
+          <Route path="/contact" element={ <Contact></Contact>}>
+         </Route>
+
+          <Route path="/blogs" element={ <OurBlogs></OurBlogs>}>
+         </Route>
+
+          <Route path="/login" element={ <Login></Login>}>
+         </Route>
+
+          <Route path="/registration" element={ <Registration></Registration>}>
+         </Route>
+
+          <Route path="*" element={<NotFound />}>
+            
+          </Route>
+      </Routes>
+      <Footer></Footer>
+
     </div>
   );
 }
